1487e226775c7584484dbcf9f6a2d1f028baae38
nancyshi <greenfavo@qq.com>
2016-07-21 18:49:23 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+##koa-blog
+基于koa.js的一个blog demo，采用MVC的方式组织代码，主要功能是对mongodb数据库的CURD操作。利用koa提供的generator写法可以很轻松地解决异步回调问题，避免层层嵌套使代码一目了然。
+##运行方式
+
+ 1. 打开mongodb数据库
+ 
+ 2. 安装依赖项
+ ```sh
+ npm install
+ ```
+ 3. 运行
+ 
+ ```sh
+ node app
+ // 打开浏览器:localhost:3000
+ ```
--- models/models.js
+++ models/models.js
@@ -22,35 +22,8 @@ var articleSchema = mongoose.Schema({
 });
 
 // 绑定模型
-// 用户模型
 var User = mongoose.model('User', userSchema);
 var Article = mongoose.model('Article', articleSchema);
-
-// 初始化
-// User.find(function(err, users){
-//     if(users.length)
-//         return;
-//     var newUser=new User({
-//         userName: 'root',
-//         password: 'root',
-//     });
-
-//     yield newUser.save();
-// });
-app.use(function *(next) {
-    var newUser=new User({
-        userName: 'root',
-        password: 'root',
-    }); 
-    yield newUser.save();
-    try {
-        this.data=yield User.find({userName:'root'});
-        console.log(this.data);
-    } catch(e) {
-        // statements
-        console.log(e);
-    }
-})
 
 // 导出模型
 exports.User = User;
